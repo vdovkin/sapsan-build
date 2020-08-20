3cc3e3c22894e6390ae99882a405c3b330fbd61b
Vdovkin Oleksandr <a.a.vdovkin@gmail.com>
2020-08-20 17:05:09 +0300

    Modifide main page

---
+++ apps.py
@@ -0,0 +1,31 @@
+APPS = [
+    # steel
+    {"link": "steelbeam", "name": "Сталева Балка", "img": "img/Solidgrey.png"},
+    {"link": "steelsection", "name": "Сталевий Переріз", "img": "img/Solidgrey.png"},
+    {"link": "steelcolumn", "name": "Сталева Колона", "img": "img/Solidgrey.png"},
+    {"link": "steeloptions", "name": "Сталь", "img": "img/Solidgrey.png"},
+    {
+        "link": "steelassortment",
+        "name": "Сортамент Металопрокату",
+        "img": "img/Solidgrey.png",
+    },
+    # concrete
+    {"link": "concretebeam", "name": "Залізобетонна балка", "img": "img/Solidgrey.png"},
+    {"link": "concreteslab", "name": "Залізобетонна плита", "img": "img/Solidgrey.png"},
+    {
+        "link": "concretesection",
+        "name": "Залізобетонний переріз",
+        "img": "img/Solidgrey.png",
+    },
+    # other
+    {
+        "link": "unitscalculator",
+        "name": "еретворювач одиниць виміру",
+        "img": "img/Solidgrey.png",
+    },
+    {
+        "link": "verticaldeflections",
+        "name": "Вертикальні Прогини",
+        "img": "img/Solidgrey.png",
+    },
+]
--- core/main/routes.py
+++ core/main/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, Blueprint
+from apps import APPS
 
 main = Blueprint("main", __name__)
 
@@ -6,4 +7,4 @@ main = Blueprint("main", __name__)
 @main.route("/")
 @main.route("/home")
 def home():
-    return render_template("main/home.html")
+    return render_template("main/home.html", apps=APPS)
