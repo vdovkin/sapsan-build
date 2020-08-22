ff96889d9733ed15d5307fe3b483f8356363ce81
Vdovkin Oleksandr <a.a.vdovkin@gmail.com>
2020-08-22 16:22:25 +0300

    Add about and contact pages

--- core/main/routes.py
+++ core/main/routes.py
@@ -8,3 +8,13 @@ main = Blueprint("main", __name__)
 @main.route("/home")
 def home():
     return render_template("main/home.html", apps=APPS)
+
+
+@main.route("/about")
+def about():
+    return render_template("main/about.html", title="Про сервіс")
+
+
+@main.route("/contact")
+def contact():
+    return render_template("main/contact.html", title="Контакти")
